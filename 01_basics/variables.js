ebadaa77ddcca1e0e26b34921b6be2bba4310c76
wpregal <123128913+wpregal@users.noreply.github.com>
2023-07-10 02:43:10 +0000

    variable done

---
+++ 01_basics/variables.js
@@ -0,0 +1,34 @@
+const accountid = 525252
+let accountEmail = "my@gmail.com"
+var accountPassword = "121212"
+cityName = "lahore"
+
+
+accountEmail = "usman@ff.com"
+accountPassword = "454545"
+cityName = "okara"
+
+console.table([accountid, accountEmail, accountPassword, cityName])
+
+
+/*
+
+prefer not to use var 
+because of issuse in block scope / funcation scope 
+
+*/
+//console.log(accountid);
+
+
+
+let x = "23"
+let y = "55"
+x
+z = x + y
+
+const f = "5"
+
+a = z / f
+
+g = 555/5
+console.log(a)
